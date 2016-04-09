4140c47ebf060e27e3926da521bc98dc17a6aa2e
Frost <cpaccione77@gmail.com>
2016-04-09 02:19:39 -0500

    Multiple Updates
    - Added SASS
    - Added background images to all page

--- MTL/page-home.php
+++ MTL/page-home.php
@@ -11,11 +11,38 @@ Template Name: Home Page
 
 <?php get_header(); ?>
 
-<section class="hero-header">
+<section class="hero-header" style="background-image: url(<?php the_field('hero_header_image'); ?>);">
 	<div class="hero-copy">
-		<h1>WELCOME</h1>
+		<h1><?php the_field('hero_header_copy_overlay') ;?></h1>
 	</div>
 </section>
+
+<section class="intro-copy">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2>MTL</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p><?php the_field('intro_copy'); ?></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<button>Listen Now</button>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="news">
+	<div></div>
+	<div></div>
+	<div></div>
+</section>
+
 
 
 
---
+++ MTL/page-media.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+Template Name: Media
+
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<section class="hero-header" style="background-image: url(<?php the_field('hero_header_image'); ?>);">
+	<div class="hero-copy">
+		<h1><?php the_field('hero_header_copy_overlay') ;?></h1>
+	</div>
+</section>
+
+
+
+<?php get_footer(); ?>
