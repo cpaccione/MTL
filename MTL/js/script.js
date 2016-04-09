23929505136de398d3b0f95f7c03dc7913b5bf3b
Frost <cpaccione77@gmail.com>
2016-04-09 00:52:49 -0500

    For those about to rock...

---
+++ MTL/js/script.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+
+	// Add bootstrap's 'img-responsive' class to all images
+  $('img').addClass('img-responsive');
+
+});
---
+++ MTL/page-home.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+Template Name: Home Page
+
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<section class="hero-header">
+	<div class="hero-copy">
+		<h1>WELCOME</h1>
+	</div>
+</section>
+
+
+
+<?php get_footer(); ?>
