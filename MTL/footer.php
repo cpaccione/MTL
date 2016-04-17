fc7ffd2628f50620b0a98570dc14a479156b2ad3
Frost <cpaccione77@gmail.com>
2016-04-16 21:57:46 -0500

    Footer copy and layout updates

--- MTL/footer.php
+++ MTL/footer.php
@@ -1,16 +1,14 @@
 	<footer>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-8">
 					<div class="footer-copy">
-						<small>
-							&copy; AND &copy; 2015 MARK THOMAS LAMBERT MUSIC LLC<br>
-							WARNING: ALL RIGHTS RESERVED.  UNAUTHORIZED REPRODUCTION IS A VIOLATION OF APPLICABLE LAWS AND IS<br>
-							STRICTLY PROHIBITED.  FBI ANTI-PIRACY WARNING: UNAUTHORIZED COPYING IS PUNISHABLE UNDER FEDERAL LAW. 
-						</small>
+						<h5>&copy; AND &copy; <?php echo date("Y"); ?> MARK THOMAS LAMBERT MUSIC LLC<h5>
+						<h6>WARNING: ALL RIGHTS RESERVED.  UNAUTHORIZED REPRODUCTION IS A VIOLATION OF APPLICABLE LAWS AND IS</h6>
+						<h6>STRICTLY PROHIBITED.  FBI ANTI-PIRACY WARNING: UNAUTHORIZED COPYING IS PUNISHABLE UNDER FEDERAL LAW.</h6>
 					</div>
 				</div>
-				<div class="col-md-6"></div>
+				<div class="col-md-4"></div>
 			</div>
 		</div>  
 	</footer>
