7229bf61b8ae8101617573fa18f6fa3074935a99
Frost <cpaccione77@gmail.com>
2016-07-17 11:11:04 -0500

    file structure - update 3

---
+++ page-about.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+
+Template Name: About
+
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+<section class="hero-header" style="background-image: url(<?php the_field('hero_header_image'); ?>);">
+	<div class="hero-copy">
+		<h1><?php the_field('hero_header_copy_overlay') ;?></h1>
+	</div>
+</section>
+
+<section class="quote">
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="quote-copy">
+					<h4>"If I heard a song I liked or if I went to a concert, that voice in the back of my head would say, wow that’s great, wouldn’t  it be fun to work on your music? But it never happened."</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+</section>
+
+<section class="about-first-copy">
+<div class="container">
+	<div class="row">
+	<div class="col-sm-6">
+		<div class="left-col">
+			<p>I've always enjoyed music and had a desire to write and perform. I still remember how much fun it was to play guitar in a band with some middle and high school buddies from Crown Point, Indiana.  During college  at Indiana University Northwest, I wrote around 10 songs ( lyrics, melody and chords - don't read/write notes)  but did not have much spare time to refine them as I was working full time as a grocery manager in a food store and going to school full time. Right after graduating in 1973, I earned a Masters of Management degree from Northwestern University (1974). Prior to graduate school, I bought a used piano thinking that I would have time to learn how to play (read and write notes) during graduate school but that was a joke (zero free time).</p>
+			<p>Shortly after graduate school, I became an industrial real estate broker with a firm in Chicago by the name of Podolsky and Associates which one year later merged with a large Chicago firm by the name of Arthur Rubloff and Company. I stayed at Rubloff for nine years as an industrial broker and developer. In 1984, my partner, Craig Whitehead, and I formed The Crown Group (also a Rubloff alum and from Crown Point, Indiana) www.thecrowngroup.com, a commercial development, brokerage, and management company located in Hoffman Estates, IL a suburb of Chicago.</p>
+		</div>
+	</div>
+
+	<div class="col-sm-6">
+		<div class="right-col">
+			<p>Along the way my wife of 42 years, Deborah, and I raised 3 children, Andrew, Leah and Dana, now all in their 30's.</p>
+
+			<p>So now back to the music. Between raising a family and co-running a successful business, I only occasionally would pick up my guitar and play some songs from a music book or even less frequently mess around with my own music. If I heard a song I like or if I went to a concert, that voice in the back of my head would say, wow that’s great, wouldn’t it be fun to work on your music? But it never happened.</p>
+
+			<p>In the spring of 2013, my son Andrew who had starting taking drum lessons invited my daughter Dana (who tinkers on the piano) and I to a jam session with his instructor. We individually practiced some cover songs and then got together. It was the first time I sang into a mic while playing guitar since high school. Soon after, I started brushing the dust off of my songs but still did not have a clear goal. In May of 2013, I had a heart attack (despite passing a stress test just a month before). The good news is that there was minimal damage to my heart but it now dawned on me that life is short and I better set a goal for my music.</p>
+		</div> 	
+	</div>
+</div>
+</div>
+</section>
+
+<section class="photos">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<figure class="figure">
+					<img class="figure-img center-block" src="<?php bloginfo('template_directory'); ?>/images/about-photo-past.jpg" alt="Mark Thomas Lamber - MTL">
+					<figcaption class="figure-caption text-sm-center">MUSKEGAN, MI - 19XX</figcaption>
+				</figure>
+			</div>
+			<div class="col-sm-6">
+				<figure class="figure">
+					<img class="figure-img center-block" src="<?php bloginfo('template_directory'); ?>/images/about-photo-present.jpg" alt="Mark Thomas Lambert - MTL">
+					<figcaption class="figure-caption text-sm-center">NASHVILLE, TN - 2015</figcaption>
+				</figure>
+			</div>
+		</div>
+		<hr>
+	</div>
+</section>
+
+<section class="about-second-copy">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<p>Once home, I decided that my goal was to write a minimum of 13 songs that would be refined enough to copyright. Once copyrighted, I wasn’t sure how I would go about taking my songs to the next step, i.e. hire someone to create a lead sheet or hire a band, etc.  Simultaneously, I started reading books about the music business. I wanted to learn how one submits their songs for consideration and to who and at what level of completion. As luck would have it, I found a reference to Beaird Music Group www.beairdmusicgroup.com (Beaird) out of Nashville, TN. Beaird Music Group is a premiere Demo recording company but the quality of their final product is album ready and they have produced songs heard on the radio.</p>
+
+				<p>Over a period of a year or so, I recorded 13 songs I wrote at Beaird. Larry Beaird, an accomplished musician in his own right, charted each song (a one page road map for the musicians) and then the music was tracked (each instrument has its own digital track) with some of the finest session musicians in Nashville.  I chose to sing lead on my songs (what the heck it's my dream) but hired a professional singer by the name of Adam Cunningham out of Nashville to sing backgrounds. I cannot tell you what a high it is to start the process by hearing just you and your guitar on a MP3 and ending the process with a song ready to put on an album, or if you’re fortunate, heard on the radio/internet.</p>
+
+				<p>Once these songs were recorded, I was trying to figure out what the next step might be. That's when I read about various sites that allow Indie artists to make their music available to the public including cd baby www.cdbaby.com. Once I studied this proposition, I figured why not create an album and put it out there. I asked my daughter Dana, who is a graphic artist, to create the cover for my first album, aptly named “Never Too Late.” Six of the 13 songs are from my younger days and the remaining seven are from my current years. I have to admit that I’m learning every day about the music business process but enjoying the journey.</p>
+
+				<p>I like to write songs about love, emotions, relationships, life and life's lessons that are foot tapping and tell a story (sometimes with a serious element and other times just intended to be fun).  Depending on the song, I believe my music can fall in the pop- rock, soft rock, or country categories. My primary music influences come from The Beatles, Elvis Presley and Johnny Cash (showing my age), however, there are several other artists on my playlist that I enjoy listening to.  I feel that my songs are primarily directed to my Baby Boomer generation.  I see them as a second chance to relive the genre of music we grew up with.</p>
+
+				<p>I like to write songs about love, emotions, relationships, life and life's lessons that are foot tapping and tell a story (sometimes with a serious element and other times just intended to be fun).  Depending on the song, I believe my music can fall in the pop- rock, soft rock, or country categories. My primary music influences come from The Beatles, Elvis Presley and Johnny Cash (showing my age), however, there are several other artists on my playlist that I enjoy listening to.  I feel that my songs are primarily directed to my Baby Boomer generation.  I see them as a second chance to relive the genre of music we grew up with.</p>
+
+				<p>If you happen to enjoy my music please send me an email.</p>
+
+ 				<p>Your friend,</p>
+
+				<p>Mark Thomas Lambert</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+
+
+
+<?php get_footer(); ?>
