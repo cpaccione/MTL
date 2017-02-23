052343206a01b99cdc75c204f62c8095eda185bf
Frost <cpaccione77@gmail.com>
2017-02-23 01:13:49 -0600

    About and Press page
    Finished the CSS for the about page, and complete the Press page.

--- page-about.php
+++ page-about.php
@@ -54,11 +54,11 @@ Template Name: About
 		</div>
 	</section>
 
-	<section class="mtl-history">
+	<!-- page title section -->
+	<section class="page-title section-background">
 		<div class="container">
-
 			<div class="row">
-				<div class="col-sm-12">
+				<div class="col-md-12">
 					<div class="underline-wrap">
 						<h2>learn more</h2>
 						<div class="line">
@@ -67,35 +67,40 @@ Template Name: About
 					</div>
 				</div>
 			</div>
+		</div>
+	</section>
+
+	<section class="mtl-history">
+		<div class="container">
 
 			<div class="row">
-				<div class="col-sm-6">
+				<div class="col-md-6">
 					<p>In four decades, singer-songwriter Mark Thomas Lambert raised three children, kept the fire going in a 43-year marriage, and became a successful industrial real estate broker and, for the last 33 years, business owner. The one thing he didn’t do is share the wonderful melodies and song ideas that had been rattling around in his head since his 1960s garage band days.</p>
 					<p>Now, at 65 years young, Mark welcomes a new era as a prolific singer-songwriter with the good old rockin’ debut, Never Too Late.</p>
 				</div>
-				<div class="col-sm-6">
+				<div class="col-md-6">
 					<figure class="figure">
-						<img class="figure-img center-block" src="<?php bloginfo('template_directory'); ?>/images/about-photo-past.jpg" alt="Mark Thomas Lamber - MTL">
+						<img class="figure-img" src="<?php bloginfo('template_directory'); ?>/images/about-photo-past.jpg" alt="Mark Thomas Lamber - MTL">
 						<figcaption class="figure-caption text-sm-left">Muskegan, MI - 1964</figcaption>
 					</figure>
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="col-sm-6">
+				<div class="col-md-6">
 					<figure class="figure">
-						<img class="figure-img center-block" src="<?php bloginfo('template_directory'); ?>/images/about-photo-present.jpg" alt="Mark Thomas Lambert - MTL">
+						<img class="figure-img" src="<?php bloginfo('template_directory'); ?>/images/about-photo-present.jpg" alt="Mark Thomas Lambert - MTL">
 						<figcaption class="figure-caption text-sm-left">Nashville, TN - 2015</figcaption>
 					</figure>
 				</div>
-				<div class="col-sm-6">				
+				<div class="col-md-6">				
 					<p>“The album title’s message is that it’s never too late to follow your dreams,” shares the Chicago-based artist. “I’ve yearned to do this since middle school, but the years just kept sailing by—this is the time to make it happen.”</p>
 					<p>Mark’s tagline is being a baby boomer, making baby boomer music, for baby boomers. He draws from the Beatles, Elvis, and Johnny Cash to create a vibrant mix of golden oldie ballads, raucous rockabilly, and twanging pop-rock with lyrics that range from silly to sincere.</p>
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="col-sm-12">
+				<div class="col-md-12">
 					<p>pop-rock with lyrics that range from silly to sincere. 
 					Growing up, Mark processed his life in song, chronicling everything from teenage heartache to hard-to-swallow life lessons. Career demands and domestic responsibilities eventually pried him away from music. However, a few years ago a succession of epiphanies paved the way for Mark to finally record his songs.</p> 
 					<p>In the spring of 2013, one of Mark’s children invited him to a jam session. It was the first time he sang into a microphone since high school. Months later, he survived a heart attack.  “That’s when it dawned on me that life is short and I better pursue my music now,” recalls Mark.</p>
@@ -103,7 +108,7 @@ Template Name: About
 			</div>
 
 			<div class="row">
-				<div class="col-sm-12">
+				<div class="col-md-12">
 					<aside>
 						<p>That’s when it dawned on me that life is short and I better pursue my music now.</p>
 					</aside>
@@ -111,7 +116,7 @@ Template Name: About
 			</div>
 
 			<div class="row">
-				<div class="col-sm-12">
+				<div class="col-md-12">
 					<p>With unflinching determination, Mark began to dismantle the obstacles that kept him from music. “One thing that held me back is that I didn’t fully understand the process of taking a song from start to finish,” admits Mark. Through reading about the music business, he stumbled upon Beaird Music Group in Nashville. The company is a one-stop production house staffed with hotshot musicians that can take acoustic-only songs and blossom them into professionally tracked recordings. In 2015, Mark made his first pilgrimage to Nashville to work with Beaird Music Group. Six trips later, he has a 38-song catalog of work that spans three albums.</p>
 					<p>His first album installment, Never Too Late, is a time capsule of invigorating 1960s-style rock n’ roll with five of the 13 songs dating back to Mark’s teenage years. The revved up twang rocker “I'm Insecure Immature Inefficient Inconsistent” opens the record, and has the distinction of being the very first song Mark wrote 47 years ago. It’s a heart-on-his-sleeves ode to his then girlfriend, now wife, that fits perfectly alongside the newly written eight original compositions.</p> 
 					<p>In surveying the album, it’s remarkable to note how Mark’s optimism, commitment to sincere sentiments, and love of that fist wave of rock n’ roll never waned over the decades. Other Never Too Late album highlights include the empowering folk-rocker “Life Is What You Make It,” and the euphoric Beatles-esque numbers “It’s Nice To” and “One Guy Gal.”</p>
