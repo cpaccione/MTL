57c36a2c8c00bc5179797089092c6e18b39acb53
Frost <cpaccione77@gmail.com>
2017-05-23 00:45:02 -0500

    Connected subscriber form
    I added the MailChimp code back to the appropriate page, because MTL
    accidentally deleted it.

--- page-about.php
+++ page-about.php
@@ -150,48 +150,49 @@ Template Name: About
 			</div>
 			<div class="row">
 				<div class="col-sm-12">
-					<!-- Begin MailChimp Signup Form -->
+				<!-- Begin MailChimp Signup Form -->
 					<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
 					<style type="text/css">
-						#mc_embed_signup {
-							background: #A6CBF0;
-							clear:left;
-							font-size:: 14px;
-							font-family: 'Roboto', sans-serif;
-							width: 100%;
-						}
+						  #mc_embed_signup {
+						    background: #A6CBF0;
+						    clear:left;
+						    font-size:: 14px;
+						    font-family: 'Roboto', sans-serif;
+						    width: 100%;
+						  }
 
-						#mc_embed_signup input.email {
-							font-family: 'Roboto', sans-serif;
-							height: 44px;
-						}
+						  #mc_embed_signup input.email {
+						    font-family: 'Roboto', sans-serif;
+						    height: 44px;
+						  }
 
-						#mc_embed_signup .button {
-							font-family: 'Roboto', sans-serif;
-							background-color: transparent;
-							border: 1px solid #ffffff;
-							height: 44px;
-							font-size: 1.125rem;
-						}
+						  #mc_embed_signup .button {
+						    font-family: 'Roboto', sans-serif;
+						    background-color: transparent;
+						    border: 1px solid #ffffff;
+						    height: 44px;
+						    font-size: 1.125rem;
+						  }
 
-						#mc_embed_signup .button:hover {
-							background-color: #2C4E86;
-						}
+						  #mc_embed_signup .button:hover {
+						    background-color: #2C4E86;
+						  }
+					/*	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}*/
 						/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
 						   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 					</style>
+
 					<div id="mc_embed_signup">
-					<form action="//markthomaslambert.us15.list-manage.com/subscribe/post?u=af6afaec5ca4509b0e0c26bb0&amp;id=3330f8fd2f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					    <div id="mc_embed_signup_scroll">
-						<!-- <label for="mce-EMAIL"></label> -->
-						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required>
-					    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-					    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_af6afaec5ca4509b0e0c26bb0_3330f8fd2f" tabindex="-1" value=""></div>
-					    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-					    </div>
-					</form>
+						<form action="//markthomaslambert.us15.list-manage.com/subscribe/post?u=af6afaec5ca4509b0e0c26bb0&amp;id=d890dd7f95" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						    <div id="mc_embed_signup_scroll">
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+						    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_af6afaec5ca4509b0e0c26bb0_d890dd7f95" tabindex="-1" value=""></div>
+						    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						    </div>
+						</form>
 					</div>
-					<!--End mc_embed_signup-->
+				<!--End mc_embed_signup-->
 				</div>
 			</div>
 
